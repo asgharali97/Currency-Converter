2e8e211ca726c6ba738a650ad887d9828071763f
asgharali97 <asgharalifs48@gmail.com>
2024-08-12 16:16:07 -0700

    Added theme Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Input from "./componets/Input";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import { ThemeProvider } from "./Context/ThemeContext";
+import Btn from "./componets/Btn";
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
@@ -20,10 +22,25 @@ function App() {
   const convertCurrency = () => {
     setConvertedAmount((amount * currencyInfo[to]).toFixed(2))
   };
+
+  // Theme Function 
+  const [ThemeMode,setThemeMode] = useState("light")
+
+  const darkTheme = ()=>{
+    setThemeMode('dark')
+  }
+  const lightTheme = ()=>{
+    setThemeMode('light')
+  }
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light','dark')
+    document.querySelector('html').classList.add(ThemeMode)
+  },[ThemeMode])
   return (
     <>
+    <ThemeProvider value={{ThemeMode,darkTheme,lightTheme}}>
       <div
-        className="bg-gray-700 w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        className="bg-gray-700 w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat dark:bg-gray-800 dark:border-gray-700"
         style={{
           backgroundImage:
             'url("https://t3.ftcdn.net/jpg/05/74/79/80/360_F_574798026_iEIdURVR3yieUYcn2tQYakrSYB999s5k.jpg")',
@@ -31,6 +48,7 @@ function App() {
       >
         <div className="w-full">
           <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <Btn/>
             <form
               onSubmit={(e) => {
                 e.preventDefault();
@@ -52,7 +70,7 @@ function App() {
                   type="button"
                   className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-[#384f5b] text-white px-2 py-0.5"
                   onClick={swap}
-                >
+                  >
                   swap
                 </button>
               </div>
@@ -76,6 +94,7 @@ function App() {
           </div>
         </div>
       </div>
+                  </ThemeProvider>
     </>
   );
 }
